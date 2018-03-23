29c9f4d605854e0af21dc1dc63dd58d0e06b23c7
Nitin <ck.nitin@outlook.com>
2018-03-23 10:27:46 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { AngularFireDatabase } from 'angularfire2/database';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  countries: any[];
+  constructor(private db: AngularFireDatabase) {
+    this.db.list('/countries').valueChanges().subscribe(countries => {
+      this.countries = countries;
+      console.log(this.countries);
+    });
+   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCnKQ2mE-EC1AAyih2GOmI_oNyjsgDTG0Q',
+    authDomain: 'fir-demo-816c7.firebaseapp.com',
+    databaseURL: 'https://fir-demo-816c7.firebaseio.com',
+    projectId: 'fir-demo-816c7',
+    storageBucket: 'fir-demo-816c7.appspot.com',
+    messagingSenderId: '558363124304'
+    }
 };
